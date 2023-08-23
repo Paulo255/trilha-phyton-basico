f8cdf2e832e1a7908dd1e564ac79e089233fe92f
Paulo <111578715+Paulo255@users.noreply.github.com>
2023-08-22 21:47:57 -0300

    Add files via upload

---
+++ estrutura_repetição_while.py
@@ -0,0 +1,13 @@
+opcao = -1
+
+while opcao != 0 : 
+   opcao= int(input("[1] Sacar \ n[2] Extrato\ n[3] Sair \n: "))
+
+if opcao == 1:
+   print("Sacando...")
+
+elif opcao == 2:
+   print("Extrato em andamento...")
+
+else:
+   print("Obrigado por utilizar nosso sistemas bancario volte sempre!")
